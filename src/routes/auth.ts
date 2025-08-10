88c16216eda86c6c36c7c0fad4decd76ae47c059
AsbDaryaee <amirmoa76@gmail.com>
2025-08-10 20:32:04 +0330

    feat: add refresh to the auth routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
-import { register, login, logout } from "../controllers/authController";
+import {
+  register,
+  login,
+  logout,
+  refresh,
+} from "../controllers/authController";
 import { authenticateToken } from "../middleware/auth";
 
 const router = Router();
@@ -9,5 +14,7 @@ router.post("/register", register);
 router.post("/login", login);
 
 router.post("/logout", authenticateToken, logout);
+
+router.post("/refresh", authenticateToken, refresh);
 
 export default router;
